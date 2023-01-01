b5a87f9f190538f04eecce8b577abc36c299aaa4
Francisco Jose <lopezozunafranciscojose17@gmail.com>
2023-01-01 23:41:30 +0800

    Sanitize date, set security header and security for xss

--- index.ts
+++ index.ts
@@ -3,11 +3,15 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
+import ExpressMongoSanitize from 'express-mongo-sanitize';
+import helmet from 'helmet';
 import morgan from 'morgan';
 
 import connectDB from './config/db.js';
 import errorHandler from './middleware/errorHandler.js';
 import authRouter from './routes/auth.js';
+
+const xss = require("xss-clean");
 
 // Load env vars
 dotenv.config({ path: "./config/.env" });
@@ -22,6 +26,17 @@ app.use(cookieParser());
 
 // Enable CORS
 app.use(cors());
+
+// Sanitize data
+app.use(ExpressMongoSanitize());
+
+// Set security header
+app.use(helmet());
+
+// Security for XSS
+app.use(xss());
+
+// Prevent http param pollution
 
 // Dev logging middleware
 if (process.env.NODE_ENV === "development") {
@@ -48,5 +63,5 @@ process.on("unhandledRejection", (err: Error, promise) => {
   console.log(colors.red(err.message));
 
   // close server and exit
-  // server.close(() => process.exit(1));
+  server.close(() => process.exit(1));
 });
